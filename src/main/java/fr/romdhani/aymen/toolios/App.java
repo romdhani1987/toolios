08bcfcff493489a5624301281af6bc3f8f999be8
Romdhani <romdhani@live.fr>
2019-07-30 10:26:29 +0200

    [Toolios] initial import

---
+++ src/main/java/fr/romdhani/aymen/toolios/App.java
@@ -0,0 +1,13 @@
+package fr.romdhani.aymen.toolios;
+
+/**
+ * Toolios
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Start Toolios!" );
+    }
+}
