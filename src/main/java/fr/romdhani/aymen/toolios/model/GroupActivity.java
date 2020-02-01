8bcf47c216d50eb6adb6d2e8941a1f039a004c0a
Romdhani <romdhani@live.fr>
2020-02-01 09:15:03 +0100

    [Toolios] orm

---
+++ src/main/java/fr/romdhani/aymen/toolios/model/GroupActivity.java
@@ -0,0 +1,14 @@
+package fr.romdhani.aymen.toolios.model;
+
+public enum GroupActivity {
+    APPLICATION("application"), DIRECTION("direction"), RESEARCH("research"),SOFTWARE ("software"),COMMERCIAL("commercial");
+    private String    name;
+
+    GroupActivity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
