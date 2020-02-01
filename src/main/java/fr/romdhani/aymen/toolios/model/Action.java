8088b6cd3b7c5b8fa763a8c28c69f2c5df572114
Romdhani <romdhani@live.fr>
2020-02-01 11:32:18 +0100

    [Toolios] Orm...

---
+++ src/main/java/fr/romdhani/aymen/toolios/model/Action.java
@@ -0,0 +1,78 @@
+package fr.romdhani.aymen.toolios.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author aromdhani
+ */
+public class Action implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String title;
+    private String description ;
+    private Long id ;
+    private  User author;
+    private Date startDate;
+    private Date plannedDate;
+    
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getPlannedDate() {
+        return plannedDate;
+    }
+
+    public void setPlannedDate(Date plannedDate) {
+        this.plannedDate = plannedDate;
+    }
+
+
+    public Action() {
+    }
+
+    public Action(String name, String description, User author) {
+        this.title = name;
+        this.description = description;
+        this.author = author;
+            }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+}
+
