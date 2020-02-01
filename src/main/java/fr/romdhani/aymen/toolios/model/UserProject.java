4e740bcd9d625414f4ff474914e9204cbfcdefe2
Romdhani <romdhani@live.fr>
2020-02-01 13:12:40 +0100

    [Toolios] reformat code

---
+++ src/main/java/fr/romdhani/aymen/toolios/model/UserProject.java
@@ -0,0 +1,34 @@
+package fr.romdhani.aymen.toolios.model;
+
+import java.io.Serializable;
+/**
+ * @author aromdhani
+ */
+public class UserProject implements Serializable {
+    private Project project;
+    private User user;
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public UserProject() {
+    }
+
+    public UserProject(Project project, User user) {
+        this.project = project;
+        this.user = user;
+    }
+}
