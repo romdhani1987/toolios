4688838cea50ecd7604bce6f8408aca6a30597da
Romdhani <romdhani@live.fr>
2020-02-04 22:54:20 +0100

    [Toolios] init orm code

---
+++ src/main/java/fr/romdhani/aymen/toolios/orm/Request.java
@@ -0,0 +1,73 @@
+package fr.romdhani.aymen.toolios.orm;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ *
+ * @author aromdhani
+ */
+@Entity
+@Table(name = "request")
+public class Request implements Serializable {
+    private static final long serialVersionUID = 1L;
+     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "creation_time")
+    private Timestamp creationTimestamp = new Timestamp(new Date().getTime());
+
+    private  User author;
+
+    public Request() {
+    }
+
+    public Request(String name, String description, User author) {
+        this.title = name;
+        this.description = description;
+        this.author = author;
+            }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+}
+
