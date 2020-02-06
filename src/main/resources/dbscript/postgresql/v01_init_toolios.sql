ccfdf49ab557d7e5e8c85fb49124f4f3300fa74a
Romdhani <romdhani@live.fr>
2020-02-06 22:13:51 +0100

    [Toolios] init orm code

--- src/main/resources/dbscript/postgresql/v01_init_toolios.sql
+++ src/main/resources/dbscript/postgresql/v01_init_toolios.sql
@@ -1,5 +1,5 @@
 CREATE SEQUENCE public.user_id_seq;
-CREATE TABLE public.user (
+CREATE TABLE public.user_account (
                 id BIGINT NOT NULL DEFAULT nextval('public.user_id_seq'),
                 login VARCHAR(50) NOT NULL,
 				f_name VARCHAR(50),
@@ -36,7 +36,7 @@ CREATE TABLE public.user_function (
 );
 
 CREATE SEQUENCE public.private String group_id_seq;
-CREATE TABLE public.group (
+CREATE TABLE public.user_group (
                 id BIGINT NOT NULL DEFAULT nextval('public.group_id_seq'),
                 name VARCHAR(250) ,
 				group_activity_id NOT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE public.user_responsibility (
                 name VARCHAR(250) NOT NULL,
 				start_timestamp TIMESTAMP NOT NULL,
 				stop_timestamp TIMESTAMP,
-				user_id NOT NULL,
+				user_account_id NOT NULL,
 				machine_id NOT NULL,
 				serialized_properties TEXT,
                 CONSTRAINT user_responsibility_pk PRIMARY KEY (id)
@@ -170,4 +170,80 @@ CREATE TABLE public.response (
 			    CONSTRAINT response_pk PRIMARY KEY (id)
 );
 
+ALTER TABLE public.user_account ADD CONSTRAINT address_fk
+FOREIGN KEY (address_id)
+REFERENCES public.address (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
 
+ALTER TABLE public.user_account ADD CONSTRAINT user_function_fk
+FOREIGN KEY (user_function_id)
+REFERENCES public.user_function (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.user_account ADD CONSTRAINT user_responsibility_fk
+FOREIGN KEY (user_responsibility_id)
+REFERENCES public.user_responsibility (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.user_account ADD CONSTRAINT group_fk
+FOREIGN KEY (group_id)
+REFERENCES public.user_group (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.user_group ADD CONSTRAINT group_activity_fk
+FOREIGN KEY (group_activity_id)
+REFERENCES public.group_activity (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.user_group ADD CONSTRAINT company_fk
+FOREIGN KEY (company_id)
+REFERENCES public.company (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.company ADD CONSTRAINT address_fk
+FOREIGN KEY (address_id)
+REFERENCES public.address (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.user_responsibility ADD CONSTRAINT user_account_fk
+FOREIGN KEY (user_account_id)
+REFERENCES public.user_account (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.user_responsibility ADD CONSTRAINT machine_fk
+FOREIGN KEY (machine_id)
+REFERENCES public.machine (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+
+ALTER TABLE public.project_user_account_map ADD CONSTRAINT project_project_user_map_fk
+FOREIGN KEY (project_id)
+REFERENCES public.project (id)
+ON DELETE RESTRICT
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.project_user_account_map ADD CONSTRAINT user_account_project_user_map_fk
+FOREIGN KEY (user_account_id)
+REFERENCES public.user_account (id)
+ON DELETE CASCADE
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
